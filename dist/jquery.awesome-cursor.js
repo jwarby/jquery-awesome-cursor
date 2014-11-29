caeb40b169f48ac6ec1d97e547342974f77960dd
James Warwood <james.duncan.1991@googlemail.com>
2014-11-29 17:33:05 +0800

    Bump version to v0.0.4

--- dist/jquery.awesome-cursor.js
+++ dist/jquery.awesome-cursor.js
@@ -1,4 +1,4 @@
-/*! jquery-awesome-cursor - v0.0.3 - 2014-11-29
+/*! jquery-awesome-cursor - v0.0.4 - 2014-11-29
 * https://jwarby.github.io/jquery-awesome-cursor
 * Copyright (c) 2014 James Warwood; Licensed MIT */
 ;(function(global, factory) {
